64a22742ac605b7e466f5646bd026465d9bc3904
OFIFgng <lawless0nerbx@gmail.com>
2025-09-18 00:30:32 +1000

    Update server.js

--- server.js
+++ server.js
@@ -31,3 +31,6 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('✅ Server is running. You hit the root route!');
+});
